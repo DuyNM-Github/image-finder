54bcbf819ec271b2f84e71826f29ad032b2ee69b
DuyNM <duynm@vissoft.vn>
2023-01-06 17:28:01 +0700

    Finished project

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from web_automation import automaton
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-async def base_page():
-    return {"message": "Hello"}
+async def base_page(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
 
-@app.post("/goto")
+@app.post("/testrun")
 async def test_run(url: str):
-    title = await automaton.go_to(url)
+    title = await automaton.go_to_web_and_fetch_title(url)
     return {"title": title}
+
+@app.post("/find")
+async def find_images_on_google(request: Request, prompt: str = Form()):
+    result = await automaton.find_images_on_google(prompt)
+    return templates.TemplateResponse('result.html', {'request': request, 'prompt': prompt, 'images': result})
--- web_automation/automaton.py
+++ web_automation/automaton.py
@@ -1,6 +1,11 @@
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
+from selenium.webdriver.support.expected_conditions import *
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.firefox.service import Service as FirefoxService
+from retry import retry
 
 service = FirefoxService(executable_path=GeckoDriverManager().install())
 
@@ -9,9 +14,40 @@ def _init_driver():
     ff_option.headless = True
     return webdriver.Firefox(service=service, options=ff_option)
 
-async def go_to(url: str):
+async def go_to_web_and_fetch_title(url: str):
     driver = _init_driver()
     driver.get(url)
     title = driver.title
     driver.close()
     return title
+
+async def find_images_on_google(prompt: str):
+    # Init driver and navigate to Google Image
+    driver = _init_driver()
+    driver.get("https://google.com")
+    gg_image = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div/div/div/div[1]/div/div[2]/a')
+    gg_image.click()
+
+    # Input prompt to search bar
+    search_bar = driver.find_element(By.TAG_NAME, "input")
+    search_bar.send_keys(prompt + Keys.ENTER)
+
+    # Gather images
+    img_links = []
+    image_elems = WebDriverWait(driver, timeout=5).until(lambda d: d.find_elements(By.XPATH, '/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div/div[1]/div[1]/span/div[1]/div[1]/div'))
+    for elem in image_elems:
+        elem.click()
+        side_bar = WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.ID, 'islsp'))
+        try:
+            img_region: WebElement = side_bar.find_element(By.CSS_SELECTOR, "[role='region']")
+        except NoSuchElementException:
+            break
+        target_img = img_region.find_element(By.CSS_SELECTOR, "a > img")
+        img_links.append(target_img.get_attribute("src"))
+
+    driver.close()
+
+    if img_links.__len__ == 0:
+        find_images_on_google(str)
+
+    return img_links
